fbeebe56c01b48cbb62284432c78ca861ffefa0e
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-06-09 20:28:02 -0400

    added user class and db helper functions

--- Flutter App/wifi_airflow_control/lib/db.dart
+++ Flutter App/wifi_airflow_control/lib/db.dart
@@ -1,36 +1,48 @@
+import 'dart:io';
+
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:wifi_airflow_control/user.dart';
 
 class DatabaseHelper {
-  static Future<Database> get database async {
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, 'my_database.db');
-    return openDatabase(
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _db;
+  Future<Database> get db async => _db ??= await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'users.db');
+    return await openDatabase(
       path,
       version: 1,
-      onCreate: (db, version) async {
-        await db.execute(
-            'CREATE TABLE users (uid INTEGER PRIMARY KEY, displayName TEXT, photoUrl TEXT, dampers TEXT)');
-      },
+      onCreate: _onCreate,
     );
   }
 
-  static Future<void> insertData(String data) async {
-    final db = await database;
-    await db.insert('my_table', {'data': data});
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users(
+        uid INTEGER PRIMARY KEY,
+        name TEXT,
+        photoUrl TEXT
+      )
+      ''');
   }
 
-  static Future<List<Map<String, dynamic>>> retrieveData() async {
-    final db = await database;
-    return db.query('my_table');
+  Future<List<User>> getUsers() async {
+    Database db = await instance.db;
+    var users = await db.query('users', orderBy: 'uid');
+    List<User> userList =
+        users.isNotEmpty ? users.map((e) => User.fromMap(e)).toList() : [];
+    return userList;
   }
-}
 
-Future<void> saveData() async {
-  await DatabaseHelper.insertData('value');
-}
-
-Future<void> retrieveData() async {
-  final data = await DatabaseHelper.retrieveData();
-  // Process the retrieved data
+  static Future<void> insertUser(User user) async {
+    final db = await instance.db;
+    await db.insert('user',
+        {'uid': user.uid, 'name': user.name, 'photoUrl': user.photoUrl});
+  }
 }
