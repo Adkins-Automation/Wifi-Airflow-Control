3bf4e42ef60301c412e08abac7973eb0ddf2a1be
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-10-18 08:34:46 -0400

    Update firebase.ino

--- Arduino Scripts/firebase/firebase.ino
+++ Arduino Scripts/firebase/firebase.ino
@@ -1,5 +1,7 @@
 #include "Firebase_Arduino_WiFiNINA.h"
 #include <Servo.h>
+#include <ArduinoBLE.h>
+#include <FlashStorage.h>
 
 #define DATABASE_URL "iflow-fe711-default-rtdb.firebaseio.com"
 #define DATABASE_SECRET "Y9Fjdu4CvnlpcBMtpTGx13hj4aQ5eFAwm4cQWhZn"
@@ -15,12 +17,35 @@ const String USER_ID = "ecnlzD6NLnbXqx47qcaeU2KgfDr2";
 String macAddressInDecimal; // used as damper id
 String positionPath; // firebase path to position value of this damper
 
+// Define the maximum lengths for the strings (including null terminators)
+#define MAX_SSID_LENGTH 32
+#define MAX_PASSWORD_LENGTH 64
+#define MAX_USERID_LENGTH 64
+
+// Define a struct to store SSID, password, and user ID
+struct WifiCredentials {
+  char ssid[MAX_SSID_LENGTH];
+  char password[MAX_PASSWORD_LENGTH];
+  char userId[MAX_USERID_LENGTH];
+  bool initialized;  // Flag to check if data has been written before
+};
+
+// Create an instance of the struct for flash storage
+FlashStorage(flashStorage, WifiCredentials);
+
+// Global variables for BLE
+BLEService wifiService("1800");  // Use a standard GATT service number
+BLEStringCharacteristic ssidCharacteristic("2A00", BLERead | BLEWrite, MAX_SSID_LENGTH);  // Standard GATT characteristic for Device Name, repurposed for SSID
+BLEStringCharacteristic passwordCharacteristic("2A01", BLERead | BLEWrite, MAX_PASSWORD_LENGTH);  // Another standard GATT characteristic, repurposed for password
+BLEStringCharacteristic userIdCharacteristic("2A04", BLERead | BLEWrite, MAX_USERID_LENGTH);  // Another standard GATT characteristic, repurposed for user ID
+
+// Global variables for WiFi and Firebase
+String ssid, password, userId;
+
 void setup() {
-  Serial.begin(115200);
-  while (!Serial); // Wait until the serial connection is established
-  delay(100);
-  Serial.println();
-  
+  Serial.begin(9600);
+  while (!Serial);  // Wait for serial connection
+
   byte mac[6];
   WiFi.macAddress(mac);
   
@@ -30,29 +55,49 @@ void setup() {
 
   Serial.println("Damper ID: " + macAddressInDecimal);
 
-  positionPath = USER_ID + "/" + macAddressInDecimal + "/position";
+  // 1. Attempt to retrieve SSID, password, and user ID from flash storage
+  WifiCredentials storedCredentials = flashStorage.read();
 
-  Serial.print("Connecting to Wi-Fi");
-  int status = WL_IDLE_STATUS;
-  while (status != WL_CONNECTED) {
-    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print(".");
-    delay(100);
+  if (!storedCredentials.initialized) {
+    // 2. Initialize BLE and wait for central device to provide values
+    setupBLE();
+    
+    while (!ssidCharacteristic.written() || !passwordCharacteristic.written() || !userIdCharacteristic.written()) {
+      BLE.poll();
+    }
+    
+    ssid = ssidCharacteristic.value();
+    password = passwordCharacteristic.value();
+    userId = userIdCharacteristic.value();
+
+    // 3. Store received values in flash storage
+    WifiCredentials newCredentials;
+    ssid.toCharArray(newCredentials.ssid, MAX_SSID_LENGTH);
+    password.toCharArray(newCredentials.password, MAX_PASSWORD_LENGTH);
+    userId.toCharArray(newCredentials.userId, MAX_USERID_LENGTH);
+    newCredentials.initialized = true;
+    flashStorage.write(newCredentials);
+    
+    BLE.end();  // Stop BLE services
+  } else {
+    ssid = String(storedCredentials.ssid);
+    password = String(storedCredentials.password);
+    userId = String(storedCredentials.userId);
   }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
 
-  //Provide the authentication data
-  Firebase.begin(DATABASE_URL, DATABASE_SECRET, WIFI_SSID, WIFI_PASSWORD);
-  Firebase.reconnectWiFi(true);
+  // 4. Connect to WiFi
+  connectToWiFi();
+
+  // 5. Connect and authenticate to Firebase
+  connectToFirebase();
 
   myservo.attach(9);
 
   // Set initial servo position
   myservo.write(position);
   
+  positionPath = userId + "/" + macAddressInDecimal + "/position";
+
   Firebase.setInt(fbdo, positionPath, position);
 }
 
@@ -71,4 +116,41 @@ void loop() {
   }
 
   delay(1000); // Delay to prevent rapid Firebase requests. Adjust as needed.
+}
+
+void setupBLE() {
+  if (!BLE.begin()) {
+    Serial.println("Starting BLE failed!");
+    while (1);
+  }
+  
+  BLE.setLocalName(macAddressInDecimal.c_str());
+  BLE.setAdvertisedService(wifiService);
+  
+  wifiService.addCharacteristic(ssidCharacteristic);
+  wifiService.addCharacteristic(passwordCharacteristic);
+  wifiService.addCharacteristic(userIdCharacteristic);
+  
+  BLE.addService(wifiService);
+  
+  ssidCharacteristic.setValue("");
+  passwordCharacteristic.setValue("");
+  userIdCharacteristic.setValue("");
+  
+  BLE.advertise();
+}
+
+void connectToWiFi() {
+  Serial.print("Connecting to Wi-Fi...");
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED) {
+    status = WiFi.begin(ssid.c_str(), password.c_str());
+    delay(1000);
+  }
+  Serial.println("Connected!");
+}
+
+void connectToFirebase() {
+  Firebase.begin(DATABASE_URL, DATABASE_SECRET, ssid.c_str(), password.c_str());
+  Firebase.reconnectWiFi(true);
 }
