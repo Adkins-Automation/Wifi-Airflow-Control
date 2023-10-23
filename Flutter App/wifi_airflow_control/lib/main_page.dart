dbdcf10e3ab8e69ab51302142ccb4a5082ea23d0
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-10-23 08:30:31 -0400

    modularized NewDamperDialog

--- Flutter App/wifi_airflow_control/lib/main_page.dart
+++ Flutter App/wifi_airflow_control/lib/main_page.dart
@@ -8,6 +8,7 @@ import 'package:flutter_blue/flutter_blue.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'damper.dart';
 import 'damper_slider.dart';
+import 'new_damper_dialog.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -555,74 +556,10 @@ class _MainPageState extends State<MainPage> {
 
   Future<Map<String, String?>?> _showNewDamperDialog(
       BuildContext context) async {
-    String? damperId;
-    String? ssid;
-    String? password;
-
-    damperId = "08b61f82f372";
-    // ssid = "Zenfone 9_3070";
-    // password = "mme9h4xpeq9mtdw";
-    ssid = "Adkins";
-    password = "chuck1229";
-
-    // Create TextEditingControllers for each TextField
-    final damperIdController = TextEditingController(text: damperId);
-    final ssidController = TextEditingController(text: ssid);
-    final passwordController = TextEditingController(text: password);
-
     return showDialog<Map<String, String?>>(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Enter Details'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: damperIdController,
-                onChanged: (value) {
-                  damperId = value;
-                },
-                decoration: InputDecoration(hintText: "Damper ID"),
-              ),
-              SizedBox(height: 10), // Spacer
-              TextField(
-                controller: ssidController,
-                onChanged: (value) {
-                  ssid = value;
-                },
-                decoration: InputDecoration(hintText: "SSID"),
-              ),
-              SizedBox(height: 10), // Spacer
-              TextField(
-                controller: passwordController,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: InputDecoration(hintText: "Password"),
-                obscureText: true, // To hide password input
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('CANCEL'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('OK'),
-              onPressed: () {
-                Navigator.of(context).pop({
-                  'damperId': damperId,
-                  'ssid': ssid,
-                  'password': password,
-                });
-              },
-            ),
-          ],
-        );
+        return NewDamperDialog().build(context);
       },
     );
   }
