67a06256171ce9e4ccd555f511a69f787296afe6
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-10-23 10:48:22 -0400

    wrapped dialog text

--- Flutter App/wifi_airflow_control/lib/delete_damper_dialog.dart
+++ Flutter App/wifi_airflow_control/lib/delete_damper_dialog.dart
@@ -5,19 +5,21 @@ class DeleteDamperDialog extends StatelessWidget {
   final Function(String) _onConfirm;
   final Damper _damper;
   DeleteDamperDialog(this._damper, this._onConfirm);
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
-        child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Column(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Are you sure you want to remove ${_damper.label}?'),
+              Flexible(
+                child:
+                    Text('Are you sure you want to remove ${_damper.label}?'),
+              ),
               const SizedBox(height: 15),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -42,8 +44,8 @@ class DeleteDamperDialog extends StatelessWidget {
               ),
             ],
           ),
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
