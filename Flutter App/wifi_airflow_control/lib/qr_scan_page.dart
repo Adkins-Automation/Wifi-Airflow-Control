2f0d5e40cdc625d34891d160e615373ec5ef6b5d
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-10-23 14:18:21 -0400

    added qr scan page

---
+++ Flutter App/wifi_airflow_control/lib/qr_scan_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRScanPage extends StatefulWidget {
+  @override
+  QRScanPageState createState() => QRScanPageState();
+}
+
+class QRScanPageState extends State<QRScanPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: result != null
+                  ? Text('Scanned Text: ${result!.code}')
+                  : Text('Scan a QR code'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
