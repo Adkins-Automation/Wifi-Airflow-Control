fdd3dccc7bf7f0bcf17194fced3c07cfaa90fe0d
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-10-23 06:46:10 -0400

    refactored package name
    The package name 'iFlow' isn't a snake_case identifier.

--- Flutter App/wifi_airflow_control/lib/db.dart
+++ Flutter App/wifi_airflow_control/lib/db.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:iFlow/user.dart';
+import 'package:i_flow/user.dart';
 
 class DatabaseHelper {
   DatabaseHelper._privateConstructor();
